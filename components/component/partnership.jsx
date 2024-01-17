d509d9c28d8e1644db2d084f733d3b5f76ed8e9c
Mahammed Sarfaraz Nawaz Pagala <pmdnawaz1@gmail.com>
2024-01-17 18:28:03 +0530

    fix image overlaping

--- components/component/partnership.jsx
+++ components/component/partnership.jsx
@@ -23,14 +23,14 @@ export default function Partnership() {
             <Image alt="Tesla" height="50" src={tesla} width="150" />
             <Image alt="aws" height="50" src={aws} width="150" />
             <Image alt="Adobe" height="50" src={adobe} width="150" />
-          </div>
-          <div className="flex space-x-4">
             <Image alt="IBM" height="100" src={ibm} width="150" />
             <Image alt="Microsoft" height="50" src={microsoft} width="150" />
             <Image alt="SAP" height="50" src={sap} width="150" />
             <Image alt="Tesla" height="50" src={tesla} width="150" />
             <Image alt="aws" height="50" src={aws} width="150" />
             <Image alt="Adobe" height="50" src={adobe} width="150" />
+         
+            
           </div>
         </div>
       </div>
