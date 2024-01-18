9968ba1a5cac1fd86d318e2cb2e27ddb095dec66
Mahammed Sarfaraz Nawaz Pagala <pmdnawaz1@gmail.com>
2024-01-18 18:27:11 +0530

    fix:typo

--- components/component/landing.jsx
+++ components/component/landing.jsx
@@ -62,38 +62,6 @@ export function Landing() {
       >
       </section>
 
-     <SecondCard/>
-      <OurServices />
-
-      <Partnership />
-     <TestimonialCard/>
-      <div className="responsive-iframe-container">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4332.871311807345!2d77.6091169242712!3d14.647452316587293!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb14b000140666b%3A0x8edcaad72813c8d8!2sQubicgen!5e0!3m2!1sen!2sin!4v1705479944621!5m2!1sen!2sin"
-                width="100%"
-                height="300"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                aria-hidden="false"
-                tabIndex="0"
-              />
-            </div>
-      <Footer />
-    </div>
-  );
-    <div>
-      <div className="relative">
-        <Header />
-
-      </div>
-      <section
-        className="bg-cover bg-center"
-        style={{
-          backgroundImage: "url('/placeholder.svg')",
-        }}
-      >
-      </section>
-
       <SecondCard/>
       <OurServices />
       <Partnership />
